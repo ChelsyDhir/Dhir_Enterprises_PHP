6e2ed059de4b59ea5e8dc8667dabc49762ac4913
ChelsyChelsy <chelsydhir@gmail.com>
2022-08-20 18:14:53 -0700

    Changes

---
+++ sql/Dhir.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS Dhir_Enterprises;
+CREATE DATABASE IF NOT EXISTS Dhir_Enterprises;
+USE Dhir_Enterprises;
+
+--Create table Customer
+create table Customer (
+    phone 
+    name
+    address
+    order_id
+)
